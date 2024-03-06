34f7c96dc223ed82fcd2d5d833c5ad39c5436080
Shamoulox <charly.tonnel@gmail.com>
2024-03-06 15:13:27 +0100

    ajout de La div Button Filter

--- FrontEnd/assets/Script/Data.js
+++ FrontEnd/assets/Script/Data.js
@@ -1,63 +1,56 @@
-
-
-
 // // Appel de la promess récuperation des élements
-async function loadingWorks (){
-    const response = await fetch ("http://localhost:5678/api/works")
-    const works = await response.json() 
-    console.log (works);
-    return works; //retour des données recupérées
-} 
-loadingWorks()
+async function loadingWorks() {
+  const response = await fetch("http://localhost:5678/api/works");
+  const works = await response.json();
+  console.log(works);
+  return works; //retour des données recupérées
+}
+loadingWorks();
 
 // Fonction pour créer la galerie dans le HTML
-async function createItemsHtml (){
-    const works = await loadingWorks ();
+async function createItemsHtml() {
+  const works = await loadingWorks();
 
+  // pour récupérer la  galerie et integrer Les differentes balises
+  const gallery = document.querySelector(".gallery");
 
-// pour creer la  galerie  et integrer Les differentes balises
-const gallery = document.querySelector(".gallery");
-
- works.forEach (Element  => {
+  works.forEach((Element) => {
     //creation des  constantes du html
-    const figure = document.createElement("figure")
-    const img = document.createElement("img")
-    const figcaption = document.createElement("figcaption")
-
-    // //configuration des attributs
-    figure.dataset.categoryId= Element.categoryId;
-    figure.dataset.projet = Element.id;
-    img.src = Element.imageUrl;
-    img.alt= Element.title ;
-    figcaption.innerText= Element.title;
-    
-    //ajout des élements
-    figure.appendChild(img);
-    figure.appendChild(figcaption)
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
 
     // ajout de la galerie
-
     gallery.appendChild(figure);
- })
 
+    //ajout des élements
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
 
+    // //configuration des attributs
+    figure.dataset.categoryId = Element.categoryId;
+    figure.dataset.projet = Element.id;
+    img.src = Element.imageUrl;
+    img.alt = Element.title;
+    figcaption.innerText = Element.title;
+ 
+});
 }
-
-
 createItemsHtml();
 
+// Récupération des constantes
+const portfolio = document.getElementById ("portfolio");
+const gallery = document.querySelector(".gallery");
 
+// Creation des boutons et de la div
+const buttonFilter = document.createElement("div");
+buttonFilter.classList.add ("buttonFilter");
+// Placement de  la div apres portfolio
+portfolio.insertBefore (buttonFilter, gallery);
 
-
-
-
-
-
-
-
-// .then(response => console.log(respponse)) 
+// .then(response => console.log(respponse))
 //   .then(response => {
-    
+
 //     // gerer la réponse de la requete
 //     if (!response.ok) {
 //       throw new Error("erreur reseau");
@@ -74,5 +67,4 @@ createItemsHtml();
 //     console.error("Pb requete fectch", error);
 //   });
 
-// loadWork ( ) 
-
+// loadWork ( )
